ad4583529eb079e0ac5fde12db294d88149a7f72
CarmenAegenSea <2261689550@qq.com>
2026-07-08 12:21:59 +0800

    公共配置

---
+++ server/run.bat
@@ -0,0 +1,1 @@
+java -Xms4G -Xmx7G fabric-server-mc.1.21.8-loader.0.19.3-launcher.1.1.1.jar nogui
---
+++ server/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xms4G -Xmx7G -jar fabric-server-mc.1.21.8-loader.0.19.3-launcher.1.1.1.jar nogui
